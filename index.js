7d867d169ba957b8d9a346800c17a6627fae2bcc
Dy is cool <146806223+muhammadainulfuady@users.noreply.github.com>
2025-11-08 13:12:52 +0700

    Add comments to clarify code output
    Added comments explaining the output of the code.

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ let a = 10;
 let b = 10;
 console.log(a + b);
 
-what is the input of the code above ?
+// what is the input of the code above ?
+// in this file otput being = 20
+// because 10 + 10 = 20
+
+// is easy bro
